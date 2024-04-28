26a5be3cc72ee02738a4891f34d1c56230e4a899
Alessandro <>
2024-04-28 14:22:11 +0200

    Initial Commit

---
+++ src/main/java/com/mycompany/seaside_resort/SeaSide_Resort.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.seaside_resort;
+
+/**
+ *
+ * @author Alessandro
+ */
+public class SeaSide_Resort {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

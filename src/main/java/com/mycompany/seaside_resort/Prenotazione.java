ee8a0aa34b29da79eb831dbe1ec8bc768042c1b7
Alessandro <Alessandro@Alessandro-PC>
2024-05-02 08:49:08 +0200

    Aggiunta degli attributi alla classe prenotazione e inizio scrittura menu nell'App

--- src/main/java/com/mycompany/seaside_resort/Prenotazione.java
+++ src/main/java/com/mycompany/seaside_resort/Prenotazione.java
@@ -4,10 +4,217 @@
  */
 package com.mycompany.seaside_resort;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Alessandro
  */
-public class Prenotazione {
+public class Prenotazione 
+{
+    private int idPrenotazione;
+    private int numeroOspiti;
+    private float prezzo;
+    private String nomeCliente;
+    private String trattamento;
+    private String statoPrenotazione;
+    private String livello;
+    private String vista;
+    private String Esterno;//Terrazzo o Giardino
+    private boolean disponibile;
+    private boolean tv;
+    private boolean cassaforte;
+    private int annoIN;
+    private int meseIN;
+    private int giornoIN;
+    LocalDate checkIN=LocalDate.of(annoIN, meseIN, giornoIN);
+    private int annoOUT;
+    private int meseOUT;
+    private int giornoOUT;
+    LocalDate checkOUT=LocalDate.of(annoOUT, meseOUT, giornoOUT);
+
+    public Prenotazione(int idPrenotazione, int numeroOspiti, float prezzo, String nomeCliente, String trattamento, String statoPrenotazione, String livello, String vista, String Esterno, boolean disponibile, boolean tv, boolean cassaforte, int annoIN, int meseIN, int giornoIN, int annoOUT, int meseOUT, int giornoOUT) 
+    {
+        this.idPrenotazione = idPrenotazione;
+        this.numeroOspiti = numeroOspiti;
+        this.prezzo = prezzo;
+        this.nomeCliente = nomeCliente;
+        this.trattamento = trattamento;
+        this.statoPrenotazione = statoPrenotazione;
+        this.livello = livello;
+        this.vista = vista;
+        this.Esterno = Esterno;
+        this.disponibile = disponibile;
+        this.tv = tv;
+        this.cassaforte = cassaforte;
+        this.annoIN = annoIN;
+        this.meseIN = meseIN;
+        this.giornoIN = giornoIN;
+        this.annoOUT = annoOUT;
+        this.meseOUT = meseOUT;
+        this.giornoOUT = giornoOUT;
+    }
+
+    public int getIdPrenotazione() {
+        return idPrenotazione;
+    }
+
+    public void setIdPrenotazione(int idPrenotazione) {
+        this.idPrenotazione = idPrenotazione;
+    }
+
+    public int getNumeroOspiti() {
+        return numeroOspiti;
+    }
+
+    public void setNumeroOspiti(int numeroOspiti) {
+        this.numeroOspiti = numeroOspiti;
+    }
+
+    public float getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(float prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getTrattamento() {
+        return trattamento;
+    }
+
+    public void setTrattamento(String trattamento) {
+        this.trattamento = trattamento;
+    }
+
+    public String getStatoPrenotazione() {
+        return statoPrenotazione;
+    }
+
+    public void setStatoPrenotazione(String statoPrenotazione) {
+        this.statoPrenotazione = statoPrenotazione;
+    }
+
+    public String getLivello() {
+        return livello;
+    }
+
+    public void setLivello(String livello) {
+        this.livello = livello;
+    }
+
+    public String getVista() {
+        return vista;
+    }
+
+    public void setVista(String vista) {
+        this.vista = vista;
+    }
+
+    public String getEsterno() {
+        return Esterno;
+    }
+
+    public void setEsterno(String Esterno) {
+        this.Esterno = Esterno;
+    }
+
+    public boolean isDisponibile() {
+        return disponibile;
+    }
+
+    public void setDisponibile(boolean disponibile) {
+        this.disponibile = disponibile;
+    }
+
+    public boolean isTv() {
+        return tv;
+    }
+
+    public void setTv(boolean tv) {
+        this.tv = tv;
+    }
+
+    public boolean isCassaforte() {
+        return cassaforte;
+    }
+
+    public void setCassaforte(boolean cassaforte) {
+        this.cassaforte = cassaforte;
+    }
+
+    public int getAnnoIN() {
+        return annoIN;
+    }
+
+    public void setAnnoIN(int annoIN) {
+        this.annoIN = annoIN;
+    }
+
+    public int getMeseIN() {
+        return meseIN;
+    }
+
+    public void setMeseIN(int meseIN) {
+        this.meseIN = meseIN;
+    }
+
+    public int getGiornoIN() {
+        return giornoIN;
+    }
+
+    public void setGiornoIN(int giornoIN) {
+        this.giornoIN = giornoIN;
+    }
+
+    public LocalDate getCheckIN() {
+        return checkIN;
+    }
+
+    public void setCheckIN(LocalDate checkIN) {
+        this.checkIN = checkIN;
+    }
+
+    public int getAnnoOUT() {
+        return annoOUT;
+    }
+
+    public void setAnnoOUT(int annoOUT) {
+        this.annoOUT = annoOUT;
+    }
+
+    public int getMeseOUT() {
+        return meseOUT;
+    }
+
+    public void setMeseOUT(int meseOUT) {
+        this.meseOUT = meseOUT;
+    }
+
+    public int getGiornoOUT() {
+        return giornoOUT;
+    }
+
+    public void setGiornoOUT(int giornoOUT) {
+        this.giornoOUT = giornoOUT;
+    }
+
+    public LocalDate getCheckOUT() {
+        return checkOUT;
+    }
+
+    public void setCheckOUT(LocalDate checkOUT) {
+        this.checkOUT = checkOUT;
+    }
+
+    
     
 }
--- src/main/java/com/mycompany/seaside_resort/SeaSide_Resort.java
+++ src/main/java/com/mycompany/seaside_resort/SeaSide_Resort.java
@@ -4,6 +4,11 @@
 
 package com.mycompany.seaside_resort;
 
+import Input.ConsoleInput;
+import eccezioni.FileException;
+import java.io.IOException;
+import menu.Menu;
+
 /**
  *
  * @author Alessandro
@@ -12,6 +17,80 @@ public class SeaSide_Resort {
 
     public static void main(String[] args) 
     {
-        System.out.println("Hello World!");
+        String[] vociMenu;
+        int numeroVoci=11;
+        vociMenu=new String[numeroVoci];
+        Menu menu;
+        int voceScelta;
+        int esito;
+        //Scanner tastiera=new Scanner(System.in);
+        ConsoleInput tastiera=new ConsoleInput();
+        Camera camera;
+        String nomeFile="prenotazioni.csv";
+        String nomeFileBinario="prenotazioni.bin";
+        
+        vociMenu[0]="\t--> Esci";
+        vociMenu[1]="\t--> Visualizza tutti i volumi presenti";
+        vociMenu[2]="\t--> Aggiungi volume";
+        vociMenu[3]="\t--> Visualizza volume (ripiano, posizione) ";
+        vociMenu[4]="\t--> Elimina volume (ripiano, posizione)";
+        vociMenu[5]="\t--> Mostra libri di un autore";
+        vociMenu[6]="\t--> Mostra libri presenti in ordine alfabetico di titolo";
+        vociMenu[7]="\t--> Esporta i volumi su file CSV";
+        vociMenu[8]="\t--> Importa i volumi da file CSV";
+        vociMenu[9]="\t--> Salva dati scaffale";
+        vociMenu[10]="\t--> Carica dati scaffale";
+        
+        menu=new Menu(vociMenu);
+       
+        
+        //Gestione menu
+        do
+        {
+            System.out.println("Menu:");
+            voceScelta=menu.sceltaMenu();
+            switch (voceScelta) 
+            {
+                case 0:
+                    
+                    break;
+                    
+                case 1:
+                    
+                    break;
+                    
+                case 2:
+                    
+                    break;
+                    
+                case 3:
+                    
+                    break;
+
+                case 4:
+                    
+                    break;
+
+                case 5:
+                   
+                    break;
+                case 6:
+                    
+                    break;
+                case 7:
+                    
+                    break;
+                
+                case 8:              
+                    
+                    break;
+                case 9:
+                    
+                    break;
+                case 10: 
+                    
+                    break;
+            }  
+        }while(voceScelta!=0);  
     }
 }
